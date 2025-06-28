485a75652bafbb8a3898fad08bd098760749041d
Wataru Endo <waterroo118@gmail.com>
2025-06-28 15:59:14 +0900

    ownableを追加

--- lib/shopping_app/cart.rb
+++ lib/shopping_app/cart.rb
@@ -2,7 +2,7 @@ require_relative "item_manager"
 require_relative "ownable"
 
 class Cart
-  extend Ownable
+  include Ownable
 
   include ItemManager
   
@@ -11,7 +11,7 @@ class Cart
     @items = []
   end
 
-  def items < ItemManeger
+  def items 
     # Cartにとってのitemsは自身の@itemsとしたいため、ItemManagerのitemsメソッドをオーバーライドします。
     # CartインスタンスがItemインスタンスを持つときは、オーナー権限の移譲をさせることなく、自身の@itemsに格納(Cart#add)するだけだからです。
     @items
@@ -29,12 +29,16 @@ class Cart
     return if owner.wallet.balance < total_amount
   # ## 要件
     items.each do |item|
-      owner.wallet.withdrow(item.price)
+      owner.wallet.withdraw(item.price)
   #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
       item.owner.wallet.deposit(item.price)
   #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
       item.owner = owner
     end
+
+
+    @items.clear
+  end
 
   #   - カートの中身（Cart#items）が空になること。
 
@@ -42,5 +46,6 @@ class Cart
   #   - カートのオーナーのウォレット ==> self.owner.wallet
   #   - アイテムのオーナーのウォレット ==> item.owner.wallet
   #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
-  @items.clear#   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
+ #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
+
 end
